337600ade2c045df3d0e388f6469821a33f1a7a8
Bryce B <bryce.boddy@gmail.com>
2020-08-18 10:30:18 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,6 @@
 class Author < ActiveRecord::Base
-  #Validators for Name
   validates :name, presence: true
   validates :name, uniqueness: true
-  #Validators for Phone Number
+  
   validates :phone_number, length: { is: 10 }
 end
